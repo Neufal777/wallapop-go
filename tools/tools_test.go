34c9751cc8506a3615aa0e0902320b4b8028ab64
Naoufal D <neufal777@gmail.com>
2023-06-18 21:01:29 +0200

    saving changes

--- tools/tools_test.go
+++ tools/tools_test.go
@@ -2,34 +2,7 @@ package tools
 
 import (
 	"testing"
-	"time"
 )
-
-func TestTimeFormatter(t *testing.T) {
-	// Define the test input
-	timestamp := int64(1624010403000) // June 18, 2021 12:00:03 CEST
-
-	// Define the time zone for comparison
-	loc, err := time.LoadLocation("Europe/Paris")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Call the function being tested
-	result := TimeFormatter(timestamp)
-
-	// Parse the expected time in the time zone
-	expectedTime, err := time.ParseInLocation("2006-01-02 15:04:05 MST", "2021-06-18 12:00:03 CEST", loc)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := expectedTime.Format("2006-01-02 15:04:05 MST")
-
-	if result != expected {
-		t.Errorf("Expected %s, but got %s", expected, result)
-	}
-}
 
 func TestPrettyPrintStruct(t *testing.T) {
 	type TestStruct struct {
